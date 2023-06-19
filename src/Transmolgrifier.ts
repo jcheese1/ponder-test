b94bbf498fb68b821bfdcfa37c4d915166e94a2f
jc <jcheese@treasure.lol>
2023-06-19 12:37:22 +0900

    initial commit

--- src/Transmolgrifier.ts
+++ src/Transmolgrifier.ts
@@ -1,9 +1,31 @@
 import { ponder } from "@/generated";
 
-ponder.on("Transmolgrifier:RoleAdminChanged", async ({ event, context }) => {
-  console.log(event.params);
-});
+ponder.on("Transmolgrifier:SmolRecipeAdded", async ({ event, context }) => {
+  const { Recipe, Cost, Smol } = context.entities;
 
-ponder.on("Transmolgrifier:RoleGranted", async ({ event, context }) => {
-  console.log(event.params);
+  const smol = event.params.smolData.smol;
+
+  await Smol.create({
+    id: event.params.smolRecipeId.toString(),
+    data: smol,
+  });
+
+  await Recipe.create({
+    id: event.params.smolRecipeId.toString(),
+    data: {
+      cost: event.params.smolData.smolInputAmount,
+      smol: event.params.smolRecipeId.toString(),
+    },
+  });
+
+  let costEntity = await Cost.findUnique({
+    id: event.params.smolData.smolInputAmount,
+  });
+
+  if (!costEntity) {
+    await Cost.create({
+      id: event.params.smolData.smolInputAmount,
+      data: {},
+    });
+  }
 });
